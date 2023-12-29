46c0691c8c715d64866ddf7cae04b24dfbad9e39
ben <bennubeig@outlook.com>
2023-12-29 17:26:01 +0800

    Optimized cache loading

--- host/EasyAbp.Abp.TagHelperPlus.Web.Unified/Pages/Books/Book2/ViewModels/SearchBookViewModel.cs
+++ host/EasyAbp.Abp.TagHelperPlus.Web.Unified/Pages/Books/Book2/ViewModels/SearchBookViewModel.cs
@@ -15,7 +15,7 @@ namespace EasyAbp.Abp.TagHelperPlus.Pages.Books.Book2.ViewModels
             alternativeTextPropertyName: "userName",
             hideSubText: false,
             runScriptOnWindowLoad: true, // Please set to true if the item is not in a modal.            
-            minimumInputLength: 1, // The minimum number of characters required to start a search
+            //minimumInputLength: 1, // The minimum number of characters required to start a search
             delay: 500, // The delay time between the last key stroke and the request is sent to the server
             enableCache: true // Enable cache to reduce the number of requests
         )]
